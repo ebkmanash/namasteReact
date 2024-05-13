3488532e2282994cc6d7af09fbdce2139695f8d4
bala krishna <krishnamanash@gmail.com>
2024-05-13 17:34:04 +0530

    v2 37min left

--- App.js
+++ App.js
@@ -1,3 +1,6 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
 let heading=React.createElement("h1",{},"vinayakaya namaha3")
     let root=ReactDOM.createRoot(document.getElementById("root"))
     root.render(heading);
