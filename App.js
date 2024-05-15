c43f3a62a709cae914f0f6109cc44ae267688dd1
bala krishna <krishnamanash@gmail.com>
2024-05-15 09:45:27 +0530

    video 3 last 40 min left

--- App.js
+++ App.js
@@ -2,5 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 
 let heading=React.createElement("h1",{},"vinayakaya namaha3")
+let JsxHeading=<h1 > i am jsx element</h1>
     let root=ReactDOM.createRoot(document.getElementById("root"))
-    root.render(heading);
+    root.render(JsxHeading);
