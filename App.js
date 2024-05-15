970ce7240c855729806004492827a7079722abf3
bala krishna <krishnamanash@gmail.com>
2024-05-15 16:41:37 +0530

    functional component basics

--- App.js
+++ App.js
@@ -1,7 +1,25 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
-let heading=React.createElement("h1",{},"vinayakaya namaha3")
+// let heading=React.createElement("h1",{},"vinayakaya namaha3")
+
+    
+
+let FunctComp=()=>{
+
+    return(
+        
+        <div>
+            <div>first</div>
+                    Hello
+            </div>
+    )
+}
+let Title=<h2>namaste react
+    <FunctComp/>
+    {FunctComp()}
+</h2>
+       
 let JsxHeading=<h1 > i am jsx element</h1>
     let root=ReactDOM.createRoot(document.getElementById("root"))
-    root.render(JsxHeading);
+    root.render(Title);
