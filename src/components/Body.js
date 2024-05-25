d89546d2f9b39e0fddc5a8678951b78ce1278786
bala krishna <krishnamanash@gmail.com>
2024-05-25 21:37:57 +0530

    v6 6.1 6.2 full

--- src/components/Body.js
+++ src/components/Body.js
@@ -5,15 +5,20 @@ import Shimmer from "./Shimmer";
 
 let Body=()=>{
     const [listRestData,setListRestData]=useState([])
+    const [filterSearch,setFilterSearch]=useState([])
+    const[restSearch,setRestSearch]=useState("")
    
     let fetchData= async()=>{
         let data=await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=16.693711&lng=81.0497646&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING");
     
         let json=await data.json();
-        setListRestData(json.data.cards[1].card.card.gridElements.infoWithStyle.restaurants)
+        setListRestData(json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants)
+        setFilterSearch(json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants)
+        
     }
     useEffect(()=>{
         fetchData();
+        console.log("use effect")
     },[])
 
    
@@ -21,13 +26,25 @@ let Body=()=>{
         <Shimmer/>
     ):(
         <div>
+            <div className="search">
+                <div>
+                    <input name="text" value={restSearch} onChange={(e)=>{setRestSearch(e.target.value)
+            
+                    }}></input>
+                    <button className="search-button" onClick={()=>{ 
+                       const searchList =listRestData.filter((rest)=>{return(rest.info.name.toLowerCase().includes(restSearch.toLowerCase()))})
+                       
+                       setFilterSearch(searchList)
+                    }}>Search BY Restrents</button>
+                </div>
             <button className='filter' onClick={()=>{filteredListRest=listRestData.filter (res=>res.info.avgRating>4)
                 setListRestData(filteredListRest)
             }}>
                 Top Rated Restrents
             </button>
+            </div>
             <div className='card-container'>
-                {listRestData.map(cardInfo=>{
+                {filterSearch.map(cardInfo=>{
                    return(
                         <RestCard key={cardInfo.info.id} rest={cardInfo}/>
                     )
--- src/components/RestCard.js
+++ src/components/RestCard.js
@@ -1,14 +1,14 @@
 import { CDN_CLOUD } from "../utils/constants";
 let stylecard={backgroundColor:"#f0f0f0"}
 let RestCard=({rest})=>{
-    let {name,cloudinaryImageId,cuisines,avgRating}=rest.info;
-    let {deliveryTime}=rest.info.sla;
+    let {name,cloudinaryImageId,cuisines,avgRating,sla}=rest.info;
+    // let {deliveryTime}=rest.info.sla;
         
         return(<div className='rest-card' style={stylecard}>
         <img className="restimage"src={CDN_CLOUD+cloudinaryImageId} alt="food image"></img>
         <h3 className='restname'>Name:{name}</h3>
         <h4 className='restitem'> Cuisines:{cuisines.join(", ")}</h4>
-        <h4 className='resttime'>Delivery Time: {deliveryTime} minites </h4>
+        <h4 className='resttime'>Delivery Time: {sla.deliveryTime} minites </h4>
         <h5 className='reststar'>Rating:{avgRating} star</h5>
     </div>)
 }
