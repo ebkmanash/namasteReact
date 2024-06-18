cf22a81ce56587ceb481990cc045fd25bd883517
bala krishna <krishnamanash@gmail.com>
2024-06-18 05:50:02 +0530

    state to pull up 11v p3

--- src/components/RestItemCategory.js
+++ src/components/RestItemCategory.js
@@ -1,26 +1,37 @@
-import { CDN_CLOUD } from "../utils/constants"
+import { CDN_CLOUD } from "../utils/constants";
 import { useState } from "react";
-let RestItemCategory=({data,show,setShowIndex})=>{
-    console.log("at rest item category",data)
-    
-    let expand=()=>{
-        setShowIndex();
-    }
-    return(
-        <div className="  bg-green-100 shadow-lg py-4 mx-auto my-4">
-            <div className="flex justify-between cursor-pointer" onClick={expand}><span className="font-bold">
-            {data.title} ({data.itemCards.length})
-            </span>
-            <span>⬇</span>
+let RestItemCategory = ({ data, show, setShowIndex,showIndex }) => {
+  console.log("at rest item category", data,setShowIndex,showIndex);
+
+  let expand = () => {
+    setShowIndex();
+  };
+  return (
+    <div className="  bg-green-100 shadow-lg py-4 mx-auto my-4">
+      <div className="flex justify-between cursor-pointer" onClick={expand}>
+        <span className="font-bold">
+          {data.title} ({data.itemCards.length})
+        </span>
+        <span>⬇</span>
+      </div>
+      {show &&
+        data.itemCards.map((items) => {
+          return (
+            <div className="border-b-8 py-4 flex shadow-lg">
+              <div>
+                <span>{items.card.info.name} </span>
+                <span>Rs-{items.card.info.price / 100}</span>
+                <div className="py-2 my-2">{items.card.info.description}</div>
+              </div>
+              I
+              <img
+                className="w-24 h-24"
+                src={CDN_CLOUD + items.card.info.imageId}
+              ></img>
             </div>
-            {show && data.itemCards.map((items)=>{return(<div className="border-b-8 py-4 flex shadow-lg">
-                <div><span>{items.card.info.name} </span><span>Rs-{items.card.info.price/100}</span>
-            <div className="py-2 my-2" >{items.card.info.description}</div></div>
-            I<img className="w-24 h-24" src={CDN_CLOUD+items.card.info.imageId}></img>
-            </div>)})}
-            
-            
-        </div>
-    )
-}
+          );
+        })}
+    </div>
+  );
+};
 export default RestItemCategory;
--- src/components/RestMenu.js
+++ src/components/RestMenu.js
@@ -6,33 +6,56 @@ import useRestMenu from "../utils/UseRestMenu";
 import RestItemCategory from "./RestItemCategory";
 import { useState } from "react";
 
-let RestMenu=()=>{
-    let {resId}=useParams();
-    let restMenuInfo=useRestMenu(resId);
-    console.log("res id",resId)
-    const [showIndex,setShowIndex]=useState(0)
-    if (restMenuInfo===null)
-        return <Shimmer/>
-    console.log("rest menu body")
-    let {name,cuisines,avgRating,costForTwoMessage}=restMenuInfo?.cards[2]?.card?.card?.info;
-    // console.log(restMenuInfo?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards[1]?.card?.card)
-    let {itemCards}=restMenuInfo?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards[1]?.card?.card;
-    // let {itemCards}=restMenuInfo?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards.find((c)=> c.card.card["@type"]=="type.googleapis.com/swiggy.presentation.food.v2.ItemCategory")?.card?.card;
-    console.log("menu item",restMenuInfo.cards[4].groupedCard.cardGroupMap.REGULAR.cards)
-    let category=restMenuInfo.cards[4].groupedCard.cardGroupMap.REGULAR.cards.filter((c)=>c.card?.card?.["@type"]==="type.googleapis.com/swiggy.presentation.food.v2.ItemCategory")
-    console.log("category",category)
-    return(
+let RestMenu = () => {
+  let { resId } = useParams();
+  let restMenuInfo = useRestMenu(resId);
+  console.log("res id", resId);
+  const [showIndex, setShowIndex] = useState(null);
+  if (restMenuInfo === null) return <Shimmer />;
+  console.log("rest menu body");
+  let { name, cuisines, avgRating, costForTwoMessage } =
+    restMenuInfo?.cards[2]?.card?.card?.info;
+  // console.log(restMenuInfo?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards[1]?.card?.card)
+  let { itemCards } =
+    restMenuInfo?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards[1]?.card
+      ?.card;
+  // let {itemCards}=restMenuInfo?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards.find((c)=> c.card.card["@type"]=="type.googleapis.com/swiggy.presentation.food.v2.ItemCategory")?.card?.card;
+  console.log(
+    "menu item",
+    restMenuInfo.cards[4].groupedCard.cardGroupMap.REGULAR.cards
+  );
+  let category =
+    restMenuInfo.cards[4].groupedCard.cardGroupMap.REGULAR.cards.filter(
+      (c) =>
+        c.card?.card?.["@type"] ===
+        "type.googleapis.com/swiggy.presentation.food.v2.ItemCategory"
+    );
+  console.log("category", category);
+  return (
     <div className="w-6/12  shadow-lg p4 mx-auto my-4  ">
-        <div>
-        <h1 className="p-2 m-2 font-bold flex justify-center">Name of restarent:{name}</h1>
+      <div>
+        <h1 className="p-2 m-2 font-bold flex justify-center">
+          Name of restarent:{name}
+        </h1>
         <h3 className="p-2 m-2 flex justify-center">{cuisines.join(",")}</h3>
         <h3 className="p-2 m-2 flex justify-center">Rating:{avgRating}*</h3>
-        <h3 className="p-2 m-2 flex justify-center shadow-lg">Cost For Two:{costForTwoMessage}</h3>
-        </div>
-        
-        {category.map((eachCategory,index)=>{return(<RestItemCategory  key={eachCategory?.card?.info?.name} data={eachCategory?.card?.card} show={index===showIndex?true:false} setShowIndex={()=>setShowIndex(index)}/>)})}
-                  
+        <h3 className="p-2 m-2 flex justify-center shadow-lg">
+          Cost For Two:{costForTwoMessage}
+        </h3>
+      </div>
+
+      {category.map((eachCategory, index) => {
+        return (
+          <RestItemCategory
+            key={eachCategory?.card?.info?.name}
+            data={eachCategory?.card?.card}
+            show={index === showIndex ? true : false}
+            setShowIndex={() => setShowIndex(index)}
+            showIndex={showIndex}
+          />
+        );
+      })}
     </div>
-    )
-}
+  );
+};
 export default RestMenu;
